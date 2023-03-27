4e53ebdd21b7f57f81af538dd2aa41befddad89b
Sarah Wettengeel <sarahwettengel@gmx.de>
2023-03-27 12:41:39 +0200

    remove console.log

--- pages/dashboard/AllEntries/index.js
+++ pages/dashboard/AllEntries/index.js
@@ -85,7 +85,7 @@ export default function EntriesSection({
     );
     setFilteredEntriesCount(filteredEntries.length);
   }, [formData, selectedTag]);
-  console.log(formData);
+
   return (
     <>
       <Heading>Your Mood:</Heading>
